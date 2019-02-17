2e2d17c2d00a4e62f7a9fa68914811d5295ef242
chris_home <hsiao516@gmail.com>
2019-02-18 00:34:12 +0800

    first upload

---
+++ vue-lesson/src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
+
+// new Vue({
+//   el: '#app',
+// render: function(creatElement){
+//   return creatElement(App);
+// }
+// })
